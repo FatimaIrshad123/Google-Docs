b62ce39696734ddd664ebcb9144664dec0e69072
FatimaIrshad123 <bibimemoona2017@gmail.com>
2025-02-21 21:23:36 +0500

    Added update and delete functionality

--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -3,6 +3,7 @@ import { Doc } from "../../../convex/_generated/dataModel"
 import { LoaderIcon } from "lucide-react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { DocumentRow } from "./document-row";
+import { Button } from "@/components/ui/button";
 
 interface DocumentsTableProps {
     documents: Doc<"documents">[] | undefined;
@@ -43,6 +44,13 @@ export const DocumentsTable = ({documents,loadMore,status}: DocumentsTableProps)
                     )}
                 </Table>
             )}
+            <div className="flex items-center justify-center">
+                <Button variant="ghost" size="sm" 
+                    onClick={() => loadMore(5)}
+                    disabled={status !== "CanLoadMore"}>
+                    {status === "CanLoadMore" ? "Load more" : "End of results"}
+                </Button>
+            </div>
         </div>
     )
 }
